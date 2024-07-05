c4510125b3702c9132103cd4075847b021579f30
hector <hectorrrivera1010@gmail.com>
2024-07-05 17:14:16 -0600

    add

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -4,9 +4,30 @@ import { Component } from '@angular/core';
   selector: 'app-games',
   standalone: true,
   imports: [],
-  templateUrl: './games.component.html',
+  template: `
+    <ul>
+      @for(game of games; track game.id){
+        <li>
+          {{game.name}}
+        </li>
+      }
+    </ul>
+  `,
   styleUrl: './games.component.css'
 })
 export class GamesComponent {
-
+  games = [
+    {
+      id: 1,
+      name: 'Death grips: The revenge of mc ride'
+    },
+    {
+      id: 2,
+      name: 'GTA 3'
+    },
+    {
+      id: 3,
+      name: 'Geometry dash'
+    }
+  ]
 }
